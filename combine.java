84cc66d59eff9d20d3bb4ef43d5f800337b8ff6f
deepa-124 <deepashankargodpatil2gmail.com>
2025-04-08 17:06:07 +0530

    save

--- combine.java
+++ combine.java
@@ -1,5 +1,7 @@
 public class combine {
          public static void main(String[] args) {
             System.out.println("hello guys");
+            System.out.println("hello jii");
+
          }
 }
